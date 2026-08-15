c64b0ee72c846022fbe9481677d454850dcf1d63
pallavimshr <44530247+pallavimshr@users.noreply.github.com>
2026-08-15 18:15:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (89.78%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+
+        int OR = 0;
+
+        for (int x : nums) {
+            OR = OR | x;
+        }
+
+        return OR * (1 << (nums.size() - 1));
+    }
+};
