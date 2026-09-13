6e556c5f580c19db95eb2e305e974126dfc169a5
pallavimshr <44530247+pallavimshr@users.noreply.github.com>
2026-09-13 12:01:04 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (95.66%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<int> answer(n, 1);
+
+        // Step 1: Store prefix products
+        int prefix = 1;
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        // Step 2: Multiply by suffix products
+        int suffix = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return answer;
+    }
+};
