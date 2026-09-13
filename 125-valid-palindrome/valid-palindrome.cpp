a35bbd63d6126283d7ca68ee75d021efb76362b6
pallavimshr <44530247+pallavimshr@users.noreply.github.com>
2026-09-13 15:54:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (76.99%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+
+            // Skip non-alphanumeric characters from left
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+
+            // Skip non-alphanumeric characters from right
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            // Compare lowercase characters
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
